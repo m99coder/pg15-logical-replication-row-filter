a2c3e213daa212fa63baf91023206aaf8efe80b1
Marco Lehmann <m99@posteo.de>
2023-09-25 15:53:30 +0200

    provide config file

--- sql/repl_pub.sql
+++ sql/repl_pub.sql
@@ -1,1 +1,5 @@
-SELECT version();
+-- create publication on all four pgbench tables
+-- with using a row filter
+CREATE PUBLICATION pub_bid_1
+  FOR TABLE pgbench_accounts, pgbench_branches, pgbench_history, pgbench_tellers
+  WHERE (bid = 1);
